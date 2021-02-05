02f478d3ed193eb3ae118e1c392845bd06f66625
Amit Gal <amitgalmail@gmail.com>
2021-02-05 15:50:29 +0100

    deploy with prefect cloud

--- nl_open_data/flows/run/cbs/run_test_statline_bq_flow.py
+++ nl_open_data/flows/run/cbs/run_test_statline_bq_flow.py
@@ -1,15 +1,15 @@
 from nl_open_data.config import config
 from datetime import datetime
 from prefect import Client as PrefectClient
-from dask.distributed import Client as DaskClient
+from dask.distributed import Client as DaskClient, LocalCluster
 
 # Schedules a flow-run on prefect cloud
 
 # Prefect client parameters
 TENANT_SLUG = "dataverbinders"
 
-# # dask parameters
-# IP = "127.0.0.1:8888"
+# dask parameters
+IP = "127.0.0.1:61611"
 
 # flow parameters
 DATA = ["83583NED", "83765NED"]
@@ -22,6 +22,12 @@ FORCE = True
 STATLINE_VERSION_GROUP_ID = "statline_bq"
 RUN_NAME = f"test_statline-bq_{datetime.today().date()}_{datetime.today().time()}"
 
+prefect_client = PrefectClient()  # Local api key has been stored previously
+prefect_client.login_to_tenant(tenant_slug=TENANT_SLUG)  # For user-scoped API token
+
+cluster = LocalCluster(ip=IP)
+dask_client = DaskClient(cluster)
+
 statline_parameters = {
     "ids": DATA,
     "source": SOURCE,
@@ -29,16 +35,6 @@ statline_parameters = {
     "gcp_env": GCP_ENV,
     "force": FORCE,
 }
-
-if __name__ == "__main__":
-    # cluster = LocalCluster(host=IP)
-    # dask_client = DaskClient(cluster)
-
-    prefect_client = PrefectClient()  # Local api key has been stored previously
-    prefect_client.login_to_tenant(tenant_slug=TENANT_SLUG)  # For user-scoped API token
-
-    flow_run_id = prefect_client.create_flow_run(
-        version_group_id=STATLINE_VERSION_GROUP_ID,
-        parameters=statline_parameters,
-        run_name=RUN_NAME,
-    )
+flow_run_id = prefect_client.create_flow_run(
+    version_group_id=STATLINE_VERSION_GROUP_ID, parameters=statline_parameters
+)
