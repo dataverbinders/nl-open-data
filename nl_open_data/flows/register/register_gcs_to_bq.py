e01d4be9485df2a1601b158677d019a65b5b80b5
Amit Gal <amitgalmail@gmail.com>
2021-05-02 16:36:36 +0200

    first implementation of gcs_to_bq in run_cbs_helper

--- nl_open_data/flows/register/register_gcs_to_bq.py
+++ nl_open_data/flows/register/register_gcs_to_bq.py
@@ -1,13 +1,11 @@
 # Registers a flow that takes different gcs parquet files, and creates a single dataset, where each parquet file is a table.
 
-# the config object must be imported from config.py before any Prefect imports
-from nl_open_data.config import config as CONFIG
-
 from prefect import Flow, unmapped, Parameter
 from prefect.run_configs import LocalRun
 from prefect.storage import GCS
 from prefect.executors import DaskExecutor
 
+from nl_open_data.config import config as CONFIG
 import nl_open_data.tasks as nlt
 
 with Flow("gcs_to_bq") as gcs_to_bq_flow:
